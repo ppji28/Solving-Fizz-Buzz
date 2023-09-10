7d60815d55a86fc772ae0cfc16499e4071a403b4
ppji28 <pjn4tz@gmail.com>
2023-09-10 17:18:28 +0700

    Add index Add styles Add script

---
+++ script.js
@@ -0,0 +1,26 @@
+function playFizzBuzz() {
+    // Get user input
+    const userInput = document.getElementById("userInput").value;
+    
+    // Check if the input is a number
+    if (!isNaN(userInput)) {
+        let result = "";
+
+        // Check for divisibility by 3 and 5
+        if (userInput % 3 === 0) {
+            result += "Fizz";
+        }
+        if (userInput % 5 === 0) {
+            result += "Buzz";
+        }
+
+        // Display the result in an alert
+        if (result) {
+            alert(result);
+        } else {
+            alert(userInput);
+        }
+    } else {
+        alert("Please enter a valid number.");
+    }
+}
